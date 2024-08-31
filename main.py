215a762bb52ca51369b5073806ebbc4a6bdf4997
IBK <ibukunoluwa.adetoba@gmail.com>
2024-08-31 13:05:03 +0100

    refactor: some changes made

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ while True:
         text = input("Enter the text you want to translate: ")
 
         translator = Translator(to_lang=lang)
-        translation = translator.translate(text)
+        translation: str = str(translator.translate(text))  # type: ignore
         print(translation)
     except KeyboardInterrupt:
         break
